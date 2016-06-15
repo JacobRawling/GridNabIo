3c99664845a2bfbd8464780dcf7cdc1bdfb8bbaa
JacobRawling <mrjacob3@gmail.com>
2016-06-16 00:24:03 +0100

    Added index.html and basic resizing of canvas onload

---
+++ public/js/UI.js
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+ * Author: Jacob rawling
+ * Date: 06/2016
+ * Email: Rawling.Jacob@gmail.com
+ */
+
+function initilaizeCanvas(){
+  // Get the canvas element form the page
+  var canvas = document.getElementById("canvas");
+
+  //Resize the canvas to occupy the full page
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+}
